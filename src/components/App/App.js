0a92c632815bea65667bde3545c546f1a4062153
ayan4m1 <andrew@bulletlogic.com>
2020-08-20 13:41:40 -0400

    add bouncing face

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import AnimatedBg from 'react-animated-bg';
 import { interpolateHslLong } from 'd3-interpolate';
 
+const imageHeight = 300;
+const imageWidth = 350;
+
 export class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      translate: {
+        x: 0,
+        y: 0
+      },
+      speed: {
+        x: -Math.ceil(Math.random() * 3),
+        y: -Math.ceil(Math.random() * 3)
+      }
+    };
     this.colors = [];
 
     for (let i = 0; i <= 1; i += 1 / 32.0) {
@@ -15,9 +28,41 @@ export class App extends Component {
     for (let i = 0; i <= 1; i += 1 / 32.0) {
       this.colors.push(interpolateHslLong('blue', 'red')(i));
     }
+
+    this.updateFrame = this.updateFrame.bind(this);
+    setTimeout(() => requestAnimationFrame(this.updateFrame), 100);
+  }
+
+  updateFrame() {
+    const newState = {
+      ...this.state
+    };
+
+    if (
+      this.state.translate.x <= 0 ||
+      this.state.translate.x + imageWidth >= window.innerWidth
+    ) {
+      newState.speed.x *= -1;
+    }
+
+    if (
+      this.state.translate.y <= 0 ||
+      this.state.translate.y + imageHeight >= window.innerHeight
+    ) {
+      newState.speed.y *= -1;
+    }
+
+    newState.translate = {
+      x: this.state.translate.x + newState.speed.x,
+      y: this.state.translate.y + newState.speed.y
+    };
+
+    this.setState(newState, () => requestAnimationFrame(this.updateFrame));
   }
 
   render() {
+    const { x, y } = this.state.translate;
+
     return (
       <div className="sl-root">
         <AnimatedBg
@@ -28,7 +73,17 @@ export class App extends Component {
           timingFunction="ease-in"
           // randomMode
         >
-          <h1 className="sl-text">S W A G L O R D</h1>
+          <div
+            className="sl-text"
+            style={{ transform: `translate(${x}px, ${y}px)` }}
+          >
+            <img
+              src="https://i.imgur.com/KZqhOIC.png"
+              alt="hex's face"
+              height={imageHeight}
+              width={imageWidth}
+            />
+          </div>
         </AnimatedBg>
       </div>
     );
