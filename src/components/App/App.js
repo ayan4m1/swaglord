6ce2e4dc50c6a82b00d4271785d604222bbddc3e
ayan4m1 <andrew@bulletlogic.com>
2021-10-02 16:48:28 -0400

    remove unused deps, upgrade to react 17 jsx transform

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import AnimatedBg from 'react-animated-bg';
 import { interpolateHslLong } from 'd3-interpolate';
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.scss';
-import App from './components/App/App';
+import App from 'components/App/App';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
