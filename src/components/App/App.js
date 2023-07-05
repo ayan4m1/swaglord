2b3ff0108914333bb93b072b65e87ba855368c15
ayan4m1 <andrew@bulletlogic.com>
2023-07-05 16:52:36 -0400

    various changes and improvements to the look, cycle text color

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,45 +4,57 @@ import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import {
   EffectComposer,
   Bloom,
-  DepthOfField,
   Vignette
   // eslint-disable-next-line import/no-unresolved
 } from '@react-three/postprocessing';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
+// eslint-disable-next-line import/no-unresolved
+import { Color, NearestFilter, TextureLoader } from 'three';
+
+import { interpolateHslLong } from 'd3-interpolate';
 
 // const clamp = (val, min, max) => Math.min(min, Math.max(max, val));
 const lerp = (x, y, alpha) => x * (1 - alpha) + y * alpha;
+
+const startColor = '#0000ff';
+const endColor = '#ff0000';
+const colorInterpolator = interpolateHslLong(startColor, endColor);
 
 function PointLight(props) {
   const lightRef = useRef();
 
   useFrame((scene) => {
-    const newPos = lerp(
+    lightRef.current.position.x = lerp(
       -20,
       0,
       Math.abs(Math.cos((scene.clock.getElapsedTime() - 0.5) / 2))
     );
-    lightRef.current.position.x = newPos;
   });
 
   return (
     <light {...props} ref={lightRef}>
-      <pointLight castShadow intensity={0.2} position={[10, 10, 10]} />
+      <pointLight castShadow intensity={0.6} position={[0, 10, 10]} />
     </light>
   );
 }
 
 function Text(props) {
+  const meshRef = useRef(null);
   const geom = useLoader(STLLoader, './test.stl');
 
+  useFrame((scene) => {
+    const time = scene.clock.getElapsedTime() % 10;
+    const color =
+      time >= 5
+        ? colorInterpolator((time - 5) / 5)
+        : colorInterpolator(10 - time / 5);
+    meshRef.current.material.color = new Color(color);
+  });
+
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={meshRef}>
       <primitive object={geom} attach="geometry" />
-      <meshPhysicalMaterial
-        color={'#2849b5'}
-        sheenColor={'#7889bf'}
-        sheen={5}
-      />
+      <meshPhysicalMaterial color={'#2849b5'} />
     </mesh>
   );
 }
@@ -88,26 +100,29 @@ Box.propTypes = {
 };
 
 export default function App() {
+  const tex = useLoader(TextureLoader, './fiveTone.jpg');
+
+  tex.minFilter = NearestFilter;
+  tex.magFilter = NearestFilter;
+
   return (
     <Canvas shadows>
       <ambientLight />
       <PointLight />
       <Text position={[0, 2, 0]} scale={0.1} castShadow />
-      <Box position={[-3, 0, 0]} delayTime={0} castShadow />
-      <Box position={[0, 0, 0]} delayTime={1} castShadow />
-      <Box position={[3, 0, 0]} delayTime={2} castShadow />
+      <Box position={[-3, -1, 0]} delayTime={0} castShadow />
+      <Box position={[0, -1, 0]} delayTime={1} castShadow />
+      <Box position={[3, -1, 0]} delayTime={2} castShadow />
       <mesh receiveShadow>
         <planeGeometry args={[100, 100]} />
-        <meshStandardMaterial color={'#262323'} />
+        <meshToonMaterial gradientMap={tex} color={'#262323'} />
       </mesh>
       <EffectComposer>
-        <DepthOfField
-          focusDistance={0}
-          focalLength={0.02}
-          bokehScale={2}
-          height={480}
+        <Bloom
+          luminanceThreshold={0.25}
+          luminanceSmoothing={0.9}
+          intensity={3}
         />
-        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
         <Vignette eskil={false} offset={0.1} darkness={1.1} />
       </EffectComposer>
     </Canvas>
